e4f89c7ade86676eb8ad29c67eaec6b25bcdc931
Alex <mrclean.durrant@gmail.com>
2015-10-26 18:11:38 -0600

    Initial Commit

---
+++ GameDataLib/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameGraphicsLib;
+using GameObserverLib;
+
+namespace GameDataLib
+{
+    [Serializable]
+    public class GameData
+    {
+        public GameData()
+        {
+            
+        }
+        public GameData(ObserverData observerData)
+        {
+            ObserverData = observerData;
+        }
+
+        public GameData(GraphicsData graphicsData)
+        {
+            GraphicsData = graphicsData;
+        }
+
+        public ObserverData ObserverData { get; set; }
+        public GraphicsData GraphicsData { get; set; }
+    }
+}
